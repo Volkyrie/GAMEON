736397809e8f134b18713404c6d59f524eeece75
Julie Muret <julie.muret.01@hotmail.fr>
2023-10-05 12:15:29 +0200

    Finitions et ajout des commentaires manquants

--- GameOn-website-FR/starterOnly/modal.js
+++ GameOn-website-FR/starterOnly/modal.js
@@ -85,33 +85,41 @@ function emptyError() {
 // Validation of all inputs
 formValidation.addEventListener('submit', (event) => {
   event.preventDefault();
+  // Check if first name is valid
   if (!firstInput.value.match(onlyLetters)) {
     errorMsg[0].innerText = "Syntaxe du prénom incorrecte";
   }
+  // Check if last name is valid
   else if(!lastInput.value.match(onlyLetters)) {
     emptyError();
     errorMsg[1].innerText = "Syntaxe du nom incorrecte";
   }
+  // Check if email is valid
   else if(!emailPattern.test(emailInput.value)) {
     emptyError();
     errorMsg[2].innerText = "Syntaxe de l'email incorrecte";
   }
+  // Check if birthdate is valid
   else if(!checkDate()) {
     emptyError();
     errorMsg[3].innerText = "Date incorrecte";
   }
+  // Check if number of entry is valid
   else if(!quantityInput.value.match(onlyNumbers)) {
     emptyError();
     errorMsg[4].innerText = "Veuillez entrer un nombre";
   }
+  // Check if a radio is selected
   else if(!checkRadiobox()) {
     emptyError();
     errorMsg[5].innerText = "Veuillez sélectionner un lieu";
   }
+  // Check if checkbox is checked
   else if(!btnCheck.checked){
     emptyError();
     errorMsg[6].innerText = "Veuillez cocher cette case";
   }
+  // Display validation message
   else {
     emptyError();
     confirmBg.style.display = "block";
